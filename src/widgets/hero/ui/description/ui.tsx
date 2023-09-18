a87a360c16f2f4125a6f66db0380b228af84b9d4
Temerov Anton <hurdlelightsick@gmail.com>
2023-09-18 19:09:25 +0700

    New: Add full description in movie page

--- src/widgets/hero/ui/description/ui.tsx
+++ src/widgets/hero/ui/description/ui.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { MovieDtoV13 } from "@openmoviedb/kinopoiskdev_client";
+import { usePathname } from "next/navigation";
 import { FC } from "react";
 
 interface Props {
@@ -6,14 +9,26 @@ interface Props {
 }
 export const Description: FC<Props> = (props) => {
   const { movie } = props;
-
+  const pathname = usePathname();
   return (
-    <p className="text-lg line-clamp-4">
-      {movie?.description ? (
-        movie.description
+    <>
+      {pathname === `/movie/${movie?.id}` ? (
+        <p className="text-lg">
+          {movie?.description ? (
+            movie.description
+          ) : (
+            <span>Описание отсутствует</span>
+          )}
+        </p>
       ) : (
-        <span>Описание отсутствует</span>
+        <p className="text-lg line-clamp-4">
+          {movie?.description ? (
+            movie.description
+          ) : (
+            <span>Описание отсутствует</span>
+          )}
+        </p>
       )}
-    </p>
+    </>
   );
 };
