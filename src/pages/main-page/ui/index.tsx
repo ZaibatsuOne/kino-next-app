6208fc0987642deb9b915c6ad3f08216584f7070
Temerov Anton <hurdlelightsick@gmail.com>
2023-09-20 16:11:09 +0700

    Fix: little fix types

--- src/pages/main-page/ui/index.tsx
+++ src/pages/main-page/ui/index.tsx
@@ -1,7 +1,11 @@
 import { Hero } from "@/widgets/hero";
+import { MovieDtoV13 } from "@openmoviedb/kinopoiskdev_client";
 import { NextPage } from "next";
 
-export const HomePage: NextPage = (props) => {
+interface Props {
+  movie: MovieDtoV13 | null;
+}
+export const HomePage: NextPage<Props> = (props) => {
   const { movie } = props;
   return (
     <main>
